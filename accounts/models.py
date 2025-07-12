2286ebd752d2be122fe592332acc2fe8c51089a9
balaji <balajiyalamanchili77@gmail.com>
2025-07-12 19:56:14 +0530

    made profile details show in html and also created model for profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.CharField(blank=True,max_length=10)
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+    followers = models.ManyToManyField(User,related_name='followers',blank=True)
+    following = models.ManyToManyField(User,related_name='following',blank=True)
+
+    def __str__(self):
+        return self.user.username
